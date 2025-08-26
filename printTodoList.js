17058a4b1ed376cbddb8af4f35b58eaea5bc5e2a
Sage <prestoferoce@gmail.com>
2025-08-26 19:01:45 +0900

    feat: 유효성 검사 코드 추가

--- printTodoList.js
+++ printTodoList.js
@@ -1,7 +1,10 @@
 const checked = (bool) => (bool ? "[x]" : "[ ]");
 
 export function printTodoList(arr) {
+  if (!Array.isArray(arr)) return;
+
   for (const { text, completed } of arr) {
+    if (text === "") return;
     console.log(`${checked(completed)} ${text}`);
   }
 }

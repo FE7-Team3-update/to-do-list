a0396017ee6a01fe0bc7a39b008c0aba6e83a147
Sage <prestoferoce@gmail.com>
2025-08-26 19:06:41 +0900

    feat: id 추가

--- printTodoList.js
+++ printTodoList.js
@@ -3,8 +3,8 @@ const checked = (bool) => (bool ? "[x]" : "[ ]");
 export function printTodoList(arr) {
   if (!Array.isArray(arr)) return;
 
-  for (const { text, completed } of arr) {
+  for (const { id, text, completed } of arr) {
     if (text === "") return;
-    console.log(`${checked(completed)} ${text}`);
+    console.log(`[${id}] ${checked(completed)} ${text}`);
   }
 }

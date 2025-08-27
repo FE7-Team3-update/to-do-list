c7270265caabc4632f8511be24bf51aa38b9da2d
rumpumpum <kjh04215@gmail.com>
2025-08-27 16:15:36 +0900

    fix: addTodo의 id값을 crypto.randomUUID 메서드로 생성 및 테스트 코드를 위한 리턴값 추가

--- addTodo.js
+++ addTodo.js
@@ -1,4 +1,9 @@
 export function addTodo(text, arr) {
-  const id = Date.now();
-  arr.push({ id: id, text: text, completed: false });
+  if (!Array.isArray(arr)) return;
+
+  const id = crypto.randomUUID();
+  const todo = { id: id, text: text, completed: false };
+  arr.push(todo);
+
+  return todo; // 테스트 코드 실행을 위해 반환
 }

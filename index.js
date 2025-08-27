7283f3483055ec460b079a0eef864e1927a9848c
varYeon <xjkooy100@gmail.com>
2025-08-27 17:32:38 +0900

    feat: modify,toggl 추가 (#7)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { addTodo as add } from "./addTodo.js";
 import { printTodoList as print } from "./printTodoList.js";
 import { deleteTodo as remove } from "./deleteTodo.js";
+import { modifyTodo as modify } from "./modifyTodo.js";
+import { toggleTodo as toggle } from "./toggleTodo.js";
 
 const todos = [];
 
@@ -14,4 +16,12 @@ export function printTodoList() {
 
 export function deleteTodo(id) {
   remove(id, todos);
+}
+
+export function modifyTodo(id, text) {
+  modify(id, text, todos);
+}
+
+export function toggleTodo(id) {
+  toggle(id, todos);
 }
